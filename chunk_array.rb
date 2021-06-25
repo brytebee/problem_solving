7f964f44b767895626976a2728396ca4075a6e39
Atsighi Bright <brytebee@gmail.com>
2021-06-25 10:56:05 +0100

    Add more test calls to "Chunky monkey"

--- chunk_array.rb
+++ chunk_array.rb
@@ -16,5 +16,12 @@ def chunk_array2(array, num)
   p result
 end
 
-chunk_array(["a", "b", "c", "d"], 3)
-chunk_array2(["a", "b", "c", "d"], 2)
+chunk_array(["a", "b", "c", "d"], 3); # [ [ 'a', 'b', 'c' ], [ 'd' ] ]
+chunk_array(["a", "b", "c", "d"], 2); # [ [ 'a', 'b' ], [ 'c', 'd' ] ]
+chunk_array([0, 1, 2, 3, 4, 5], 2) # [[0, 1], [2, 3], [4, 5]]
+chunk_array([0, 1, 2, 3, 4, 5], 4) # [[0, 1, 2, 3], [4, 5]]
+
+chunk_array2(["a", "b", "c", "d"], 3); # [ [ 'a', 'b', 'c' ], [ 'd' ] ]
+chunk_array2(["a", "b", "c", "d"], 2); # [ [ 'a', 'b' ], [ 'c', 'd' ] ]
+chunk_array2([0, 1, 2, 3, 4, 5], 2) # [[0, 1], [2, 3], [4, 5]]
+chunk_array2([0, 1, 2, 3, 4, 5], 4) # [[0, 1, 2, 3], [4, 5]]
